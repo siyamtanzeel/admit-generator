1246bcefd91b8c0eb54c762217a567c86bd9b345
siyamtanzeel <sttanzilas@gmail.com>
2025-11-30 02:27:02 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,176 @@
+"use client";
 import Image from "next/image";
+import { Margin, Resolution, usePDF } from "react-to-pdf";
 
 export default function Home() {
+  const { toPDF, targetRef } = usePDF({
+    filename: "example.pdf",
+    page: { margin: Margin.MEDIUM, orientation: "landscape" },
+    resolution: Resolution.HIGH,
+  });
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="bg-white p-5 flex flex-col items-center justify-center">
+      <div
+        ref={targetRef}
+        className="relative h-[770px] w-[1129px] flex items-center justify-center bg-[#0b46a518]">
+        <img
+          src="/images/logo.png"
+          className="absolute z-0 translate-x-[10%] translate-y[50%] opacity-15 w-lg"
+          alt=""
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <div className="relative z-40 m-3 h-full w-full ring-2 ring-[#0b47a5] ring-offset-4 border-4 border-[#0b47a5] flex flex-col items-center justify-start py-4">
+          {/* first row : Title Texts */}
+          <div className="grid grid-cols-7">
+            {/* first row 1st column */}
+            <div className="col-span-1">
+              <Image
+                src={"/images/logo.png"}
+                width={100}
+                height={100}
+                className="w-32 h-16 mx-auto mt-5"
+                alt="logo"
+              />
+            </div>
+            {/* first row 2nd column */}
+            <div className="col-span-5 flex flex-col items-center justify-center space-y-4 text-center py-4">
+              <p className="text-[#bb0000] opacity-70 text-xs">
+                বিসমিল্লাহির রাহমানির রাহীম
+              </p>
+              <h3 className="text-xl font-bold text-black w-full">
+                মাস্টার মোখছেদুর রহমান ফাউন্ডেশন এর পৃষ্ঠপোষকতায় <br />
+                ও <br />
+                সেনবাগ উপজেলা ওয়েলফেয়ার সোসাইটি ইউএসএ ইনক এর সহযোগিতায়
+              </h3>
+              <h1 className="text-[#0b47a5] text-4xl font-bold">
+                স্কাইরক বৃত্তি পরীক্ষা ২০২৫
+              </h1>
+              <p className="text-black text-sm font-bold">
+                মাইজদীপুর, সেনবাগ, নোয়াখালী
+              </p>
+              {/* colored box */}
+              <div className="w-[240px] h-[50px] grid grid-cols-2 items-center justify-center text-lg font-bold shadow-sm  rounded-r-2xl rounded-l-2xl text-center">
+                <span className="bg-[#066406] h-full rounded-l-2xl  border-2 border-white">
+                  প্রবেশ পত্র
+                </span>
+                <span className="bg-[#b90505] h-full rounded-r-2xl  border-2 border-white">
+                  শ্রেণীঃ ৮ম{" "}
+                </span>
+              </div>
+            </div>
+            {/* first row 3rd column */}
+            <div className="col-span-1">
+              <img
+                src={"/images/qr.png"}
+                className="w-32 h-32 mx-auto mt-5"
+                alt="qr"
+              />
+              <p className="text-xs w-32 mx-auto text-center text-[#0b47a5]">
+                আসন বিন্যাস দেখার জন্য স্ক্যান করুন।
+              </p>
+            </div>
+          </div>
+          {/* second row : form */}
+          <div className="text-black text-lg w-full px-20">
+            {/* second row 1st row */}
+            <div className="grid grid-cols-6 gap-2">
+              {/* second row 1st row 1st column */}
+              <div className="col-span-1 font-bold flex flex-col items-start justify-center">
+                <span>শিক্ষার্থীর নাম</span>
+                <span>পিতার নাম</span>
+              </div>
+              {/* second row 1st row 2nd column */}
+              <div className="col-span-2 flex flex-col items-start justify-center">
+                <span className="border-dotted border-b-2 pb-2 w-full">
+                  : মোহাম্মদ এ এন এম মিনহাজুর রহমান
+                </span>
+                <span className="border-dotted border-b-2 pb-2 w-full">
+                  : নুর আলম{" "}
+                </span>
+              </div>
+              {/* second row 1st row 3rd column */}
+              <div className="col-span-1 font-bold flex flex-col items-start justify-center">
+                <span>রোল নং </span>
+                <span>মাতার নাম</span>
+              </div>
+              {/* second row 1st row 4th column */}
+              <div className="col-span-2 flex flex-col items-start justify-center">
+                <span className="border-dotted border-b-2 pb-2 w-full">
+                  : ০১
+                </span>
+                <span className="border-dotted border-b-2 pb-2 w-full">
+                  : কানিজ সুলতানা
+                </span>
+              </div>
+            </div>
+            {/* second row 2nd row */}
+            <div className="grid grid-cols-6 gap-2">
+              {/* second row 2nd row 1st column */}
+              <div className="col-span-1 font-bold flex flex-col items-start justify-center">
+                <span>বিদ্যালয়</span>
+                <span>কেন্দ্র</span>
+              </div>
+              {/* second row 2nd row 2nd column */}
+              <div className="col-span-5 flex flex-col items-start justify-center">
+                <span className="border-dotted border-b-2 pb-2 w-full">
+                  : কল্যাণী ফজলুল হক দাখিল মাদ্রাসা, সেনবাগ
+                </span>
+                <span className="border-dotted border-b-2 pb-2 w-full">
+                  : মাইজদীপুর আশরাফুল উলুম দাখিল মাদ্রাসা
+                </span>
+              </div>
+            </div>
+            {/* second row 3rd row */}
+            <div className="grid grid-cols-6 gap-2">
+              {/* second row 3rd row 1st column */}
+              <div className="col-span-1 font-bold flex flex-col items-start justify-center">
+                <span>পরীক্ষার তারিখ </span>
+              </div>
+              {/* second row 3rd row 2nd column */}
+              <div className="col-span-2 flex flex-col items-start justify-center">
+                <span className="border-dotted border-b-2 pb-2 w-full">
+                  : ১৩ ডিসেম্বর ২০২৫
+                </span>
+              </div>
+              {/* second row 3rd row 3rd column */}
+              <div className="col-span-1 font-bold flex flex-col items-start justify-center">
+                <span>সময় </span>
+              </div>
+              {/* second row 3rd row 4th column */}
+              <div className="col-span-2 flex flex-col items-start justify-center">
+                <span className="border-dotted border-b-2 pb-2 w-full">
+                  : ১০ঃ০০ — ১১ঃ৩০
+                </span>
+              </div>
+            </div>
+          </div>
+          {/* third row : Rules and Signature */}
+          <div className="grid grid-cols-7 gap-20 mt-10 px-20">
+            <div className="col-span-5 flex flex-col items-start justify-center space-y-3">
+              <h4 className="text-[#0b47a5] text-xl">বি: দ্রঃ</h4>
+              <p className="text-sm text-black">
+                ১. পরীক্ষা শুরুর ৩০ মিনিট পূর্বে পরীক্ষার্থীকে পরীক্ষা কেন্দ্রে
+                উপস্থিত হতে হবে। <br />
+                ২. কাগজপত্র/ বইপত্র/ মোবাইল ফোন/ ক্যালকুলেটর/ স্মার্ট ঘড়ি বা
+                যেকোনো ইলেকট্রনিক ডিভাইস কেন্দ্রের ভেতর আনা যাবে না। <br />
+                ৩. পরীক্ষাকেন্দ্রের ভিতরে কোনো অসদুপায় অবলম্বন করলে পরীক্ষার্থীর
+                পরীক্ষা বাতিলসহ তার বিরুদ্ধে আইনানুগ ব্যবস্থা গ্রহণ করা হবে।
+              </p>
+            </div>
+            <div className="col-span-2 flex items-end justify-center text-center">
+              <p className="border-t-2 border-dashed py-2 text-black w-full">
+                {" "}
+                পরীক্ষা নিয়ন্ত্রক{" "}
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <button
+        type="button"
+        className="bg-indigo-500 px-3 py-2 rounded-lg mt-7"
+        onClick={() => toPDF()}>
+        Download PDF
+      </button>
     </div>
   );
 }
